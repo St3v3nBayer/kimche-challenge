e036cdb847aea87778b5c6aec3cee1cbd5561ae6
steven <stevenbayerlamprea@gmail.com>
2022-05-14 16:45:56 -0500

    Develop Page/Home

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="container">
+        <h2>Home</h2>
+        <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  )
+}
+
+export default Home
